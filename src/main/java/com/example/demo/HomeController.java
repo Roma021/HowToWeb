d967d1e06fb9f05c6c2d6f7003276be0b67fed7e
Roma021 <dtmogh@gmail.com>
2019-07-09 16:48:01 -0400

    new

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,12 +1,22 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/main")
+
+    @RequestMapping("/")
     public String homepage() {
         return "main";
+    }
+    @RequestMapping("/2")
+    public String page2(){
+        return "page2";
+    }
+    @RequestMapping("/3")
+    public String page3() {
+        return "page3";
     }
 }
